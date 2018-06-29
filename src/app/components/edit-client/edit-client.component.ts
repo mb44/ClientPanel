9099c8b3078f5d27ffa5e918aaa59143b8c653eb
Morten Beuchert <mortenbeuchert@gmail.com>
2018-06-29 23:39:45 +0200

    Load disableBalanceOnEdit from service

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../services/client.service';
 import { Client } from '../../models/Client';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-edit-client',
@@ -19,20 +20,22 @@ export class EditClientComponent implements OnInit {
     balance: 0
   };
 
-  disableBalanceOnEdit = true;
+  disableBalanceOnEdit: boolean;
 
   constructor(
     private clientService: ClientService,
     private router: Router,
     private route: ActivatedRoute,
-    private flashMessage: FlashMessagesService
+    private flashMessage: FlashMessagesService,
+    private settingsService: SettingsService
   ) { }
 
   ngOnInit() {
-      // Get id from url
-      this.id = this.route.snapshot.params['id'];
-      // Get client
-      this.clientService.getClient(this.id).subscribe(client => this.client = client );
+    this.disableBalanceOnEdit = this.settingsService.getSettings().disableBalanceOnEdit;
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => this.client = client );
   }
 
   onSubmit({value, valid}: {value: Client, valid: boolean}) {
